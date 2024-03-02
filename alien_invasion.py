96551414ae74797658c185d85e99c63e76add106
kaungkhantthar7777 <kaungkhantthar77@gmail.com>
2024-03-02 09:14:14 +0630

    added: ship class for game

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage assets and behaviour"""
@@ -16,8 +17,7 @@ class AlienInvasion:
             self.settings.screen_height
         ))
         pygame.display.set_caption("Alien Invasion")
-
-        self.bg_color = (230, 230, 230)
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -27,6 +27,7 @@ class AlienInvasion:
                     sys.exit()
 
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
             self.clock.tick(60)
 
