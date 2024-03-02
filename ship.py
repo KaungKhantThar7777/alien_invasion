035af43979f0475caaafae3ab672fb2acea042a6
kaungkhantthar7777 <kaungkhantthar77@gmail.com>
2024-03-02 11:55:30 +0630

    added: ship speed setting and bounding edge for ship

--- ship.py
+++ ship.py
@@ -6,20 +6,23 @@ class Ship:
     def __init__(self, ai_game) -> None:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
         # status for moving direction
         self.moving_status = "stop"
+        
     
     def update(self):
         """Update the ship's position based on the movement flag"""
-        if self.moving_status == "right":
-            self.rect.x += 1
-        elif self.moving_status == 'left':
-            self.rect.x -= 1
+        if self.moving_status == "right" and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_status == 'left' and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at it's current location"""
